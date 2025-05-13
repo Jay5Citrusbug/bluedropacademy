89734aa6955b241ca45a77f7e5639d178cd7d192
Jay5Citrusbug <jay5.citrusbug@gmail.com>
2025-05-13 17:09:50 +0530

    Update sendreport.ts

--- sendreport.ts
+++ sendreport.ts
@@ -25,8 +25,9 @@ const repoName = process.env.REPO_NAME || 'your-repo';
 const reportUrl = `https://${repoOwner}.github.io/${repoName}`;
 
 const msg = {
-  to: 'noam@bluedropacademy.com',
-  cc: ['jay5.citrusbug@gmail.com', 'jayshree@citrusbug.com'],
+ // to: 'noam@bluedropacademy.com',
+ // cc: ['jay5.citrusbug@gmail.com', 'jayshree@citrusbug.com'],
+  to: 'jay5.citrusbug@gmail.com'
   from: 'bluedropacademy.aws@gmail.com',
   subject: `Daily Automation Test Report - ${reportDate}`,
   html: `
