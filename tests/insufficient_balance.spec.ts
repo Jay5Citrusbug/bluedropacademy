afc9ec8f96152b5e67272df26225da50e3ef2b80
qa <qa.citrusbug@gmail.com>
2025-05-22 11:52:38 +0530

    Add workflow to main-new

--- tests/insufficient_balance.spec.ts
+++ tests/insufficient_balance.spec.ts
@@ -36,30 +36,30 @@ test('TC_26: 💰 Admin daily plan cost update and verify insufficient balance p
   await chatbotPg.close();
   await chatbotContext.close();
 
-   // ----- Admin: Reverse the balance change -----
-  const adminReverseContext = await browser.newContext();
-  const adminReversePage = await adminReverseContext.newPage();
-  const adminReverse = new Edge_case(adminReversePage); // ✅ correct class
+//    // ----- Admin: Reverse the balance change -----
+//   const adminReverseContext = await browser.newContext();
+//   const adminReversePage = await adminReverseContext.newPage();
+//   const adminReverse = new Edge_case(adminReversePage); // ✅ correct class
 
-  await adminReverse.goto();
-  await adminReverse.login(adminCredentials.email, adminCredentials.password);
-  await adminReverse.AdminreverseAmout();
-  await adminReverseContext.close();
+//   await adminReverse.goto();
+//   await adminReverse.login(adminCredentials.email, adminCredentials.password);
+//   await adminReverse.AdminreverseAmout();
+//   await adminReverseContext.close();
 
-const chatbotContext1 = await browser.newContext();
-const chatbotPage1 = await chatbotContext1.newPage();
-const chatbotLogin2 = new ChatbotLoginPage(chatbotPage1);
-const chatbotscreen = new chatbotPage(chatbotPage1);
-const form1 = new FillPersonalInfopage(chatbotPage1);
+// const chatbotContext1 = await browser.newContext();
+// const chatbotPage1 = await chatbotContext1.newPage();
+// const chatbotLogin2 = new ChatbotLoginPage(chatbotPage1);
+// const chatbotscreen = new chatbotPage(chatbotPage1);
+// const form1 = new FillPersonalInfopage(chatbotPage1);
 
-await chatbotLogin2.goto();
-await chatbotLogin2.login(chatbotCredentials.email, chatbotCredentials.password);
-await form1.fillPersonalInfo(testUserData.name, testUserData.gender);
-await chatbotPage1.evaluate(() => {
-  window.scrollTo(0, document.body.scrollHeight);
-});
-// await chatbotPage1.locator('iframe[name="htmlComp-iframe"]').contentFrame().getByRole('switch', { name: '📚 תשובה קצרה תשובה מפורטת 📄' }).isVisible();
-// await chatbotPage1.locator('iframe[name="htmlComp-iframe"]').contentFrame().getByRole('switch', { name: '📚 תשובה קצרה תשובה מפורטת 📄' }).click();
-await chatbotscreen.SubmitQuery(testInfo);
+// await chatbotLogin2.goto();
+// await chatbotLogin2.login(chatbotCredentials.email, chatbotCredentials.password);
+// await form1.fillPersonalInfo(testUserData.name, testUserData.gender);
+// await chatbotPage1.evaluate(() => {
+//   window.scrollTo(0, document.body.scrollHeight);
+// });
+// // await chatbotPage1.locator('iframe[name="htmlComp-iframe"]').contentFrame().getByRole('switch', { name: '📚 תשובה קצרה תשובה מפורטת 📄' }).isVisible();
+// // await chatbotPage1.locator('iframe[name="htmlComp-iframe"]').contentFrame().getByRole('switch', { name: '📚 תשובה קצרה תשובה מפורטת 📄' }).click();
+// await chatbotscreen.SubmitQuery(testInfo);
 
 });
